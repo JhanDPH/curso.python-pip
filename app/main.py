59d2ac18fa42a38e7be6e7d6f3439975a990e6cc
Jhan Daniel Parra <jhandanielparrah.06@hotmail.es>
2026-02-26 12:04:39 -0500

    update project app

--- app/main.py
+++ app/main.py
@@ -3,13 +3,8 @@ import read_csv
 import charts
 import pandas as pd
 
-def run():
-  '''
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  '''
 
+def run():
   df = pd.read_csv('data.csv')
   df = df[df['Continent'] == 'Africa']
 
@@ -18,22 +13,22 @@ def run():
   charts.generate_pie_chart(countries, percentages)
 
   data = read_csv.read_csv('data.csv')
+
   countries_input = input('Type Countries (separated by comma) => ')
   countries = [c.strip() for c in countries_input.split(',')]
-  for country_name in countries:
 
+  for country_name in countries:
     result = utils.population_by_country(data, country_name)
 
     if len(result) > 0:
-    country = result[0]
-    print(country)
+      country = result[0]
+      print(country)
 
-    labels, values = utils.get_population(country)
+      labels, values = utils.get_population(country)
+      charts.generate_bar_chart(country['Country'], labels, values)
+    else:
+      print(f'Country "{country_name}" not found')
 
-    charts.generate_bar_chart(country['Country'], labels, values)
-
-  else:
-    print(f'Country "{country_name}" not found')
 
 if __name__ == '__main__':
   run()
